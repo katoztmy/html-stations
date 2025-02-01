0cd268c2a23879fcd20f05ee78971dfa4ba6a2a8
katoztmy <kato.eng.dao@gmail.com>
2025-02-01 16:25:47 +0900

    1-station-done

--- src/scripts/station14.js
+++ src/scripts/station14.js
@@ -1,18 +1,41 @@
 function getData() {
-    // これから作ろうとしているもので、JavaScriptでフルネームを生成する必要が出てきました。
-    // ただ現状持っている情報では、名前と苗字をそれぞれのみしかありません。
-    // そのため、JavaScriptで連結させて生成することにしました。
-    const test = [
-        { id: 1, first_name: '優', family_name: '大木', affiliation: 'TechTrain', is_student: false },
-        { id: 2, first_name: '太郎', family_name: '山田', affiliation: 'HogeHoge大学', is_student: true }
-    ];
-    return test.map(buildFullName)
+  // これから作ろうとしているもので、JavaScriptでフルネームを生成する必要が出てきました。
+  // ただ現状持っている情報では、名前と苗字をそれぞれのみしかありません。
+  // そのため、JavaScriptで連結させて生成することにしました。
+  const test = [
+    {
+      id: 1,
+      first_name: "優",
+      family_name: "大木",
+      affiliation: "TechTrain",
+      is_student: false,
+    },
+    {
+      id: 2,
+      first_name: "太郎",
+      family_name: "山田",
+      affiliation: "HogeHoge大学",
+      is_student: true,
+    },
+  ];
+  const result = test.map((tes) => {
+    return {
+      id: test.id,
+      full_name: buildFullName(tes),
+      first_name: tes.first_name,
+      family_name: tes.family_name,
+      affiliation: tes.affiliation,
+      is_student: tes.is_student,
+    };
+  });
+  return result;
 }
 
 function buildFullName(data) {
-    // Station14の問題はこちらです。想定する答えをここを書いてください。
-    // 氏名がわかるようにしつつ、半角スペースで繋いでください。
-    // またtest配列のそれぞれのオブジェクトに対して、full_nameのプロパティが追加されるように実装すること
+  // Station14の問題はこちらです。想定する答えをここを書いてください。
+  // 氏名がわかるようにしつつ、半角スペースで繋いでください。
+  // またtest配列のそれぞれのオブジェクトに対して、full_nameのプロパティが追加されるように実装すること
+  return data.family_name + " " + data.first_name;
 }
 
 // 出力が想定される答え
--- src/scripts/station15.js
+++ src/scripts/station15.js
@@ -3,14 +3,40 @@ async function getData() {
   // ３秒後にフルネームのリストを表示されるようにしましょう。
   // 最初から表示されていると、クリアになりません。
   const userList = [
-    { id: 1, first_name: '優', family_name: '大木', affiliation: 'TechTrain', is_student: false },
-    { id: 2, first_name: '太郎', family_name: '山田', affiliation: 'HogeHoge大学', is_student: true }
+    {
+      id: 1,
+      first_name: "優",
+      family_name: "大木",
+      affiliation: "TechTrain",
+      is_student: false,
+    },
+    {
+      id: 2,
+      first_name: "太郎",
+      family_name: "山田",
+      affiliation: "HogeHoge大学",
+      is_student: true,
+    },
   ];
-  const result = await test()
-  return await result
+  const result = await test(userList);
+  console.log(result);
+  return await result;
 }
 
-function test() {
-  return
+function test(userList) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      const list = userList.map((user) => {
+        return {
+          id: user.id,
+          full_name: `${user.family_name} ${user.first_name}`,
+          first_name: user.first_name,
+          family_name: user.family_name,
+          affiliation: user.affiliation,
+          is_student: user.is_student,
+        };
+      });
+      resolve(list);
+    }, 3000);
+  });
 }
-
